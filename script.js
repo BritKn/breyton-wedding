96e61400ede7028abf059de202892409c8a23c97
Briton Kirkland <bkirkland623@gmail.com>
2026-08-26 10:16:34 -0700

    RSVP

--- script.js
+++ script.js
@@ -36,3 +36,23 @@ window.addEventListener("scroll", function () {
     }
 
 });
+const submitButton = document.getElementById("submit-rsvp");
+submitButton.addEventListener("click", function () {
+    const guestName = document.getElementById("guest-name").value;
+    const attending = document.getElementById("attending").value;
+if (guestName === "") {
+    alert("Please enter your name");
+    return;
+}
+
+const scriptURL = "https://script.google.com/macros/s/AKfycbzCAVhy7ArK1-PWwqsc04CWqZxOGqabTMVdwdtVfj-jaZ4J7j9Fsaw8Qeu6zoQ0YJM-/exec";
+fetch(scriptURL, {
+    method: "POST",
+    body: JSON.stringify ({
+        name: guestName,
+        attending: attending
+    })
+});
+console.log(guestName);
+console.log(attending);
+})
